1f2c26214fc2a60796db0bbb70f64c4733ffb84b
armedev <epiratesdev@gmail.com>
2020-12-12 11:38:45 +0530

    refactored style

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -8,6 +8,9 @@ import { auth } from "../../firebase/firebase.utils";
 
 const Profile = ({ currentUser }) => {
   let dropdownRef = useRef(null);
+  let textRef = useRef(null);
+  let signOutRef = useRef(null);
+
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
@@ -19,6 +22,12 @@ const Profile = ({ currentUser }) => {
         visibility: "visible",
         ease: Power3.easeInOut,
       });
+      gsap.to([textRef,signOutRef],{
+        opacity: 1,
+        duration: 0.5,
+        delay: 0.5,
+        ease: Power3.easeInOut
+      })
     } else {
       gsap.to(dropdownRef, {
         height: "0px",
@@ -31,6 +40,11 @@ const Profile = ({ currentUser }) => {
         visibility: "hidden",
         delay: 1,
       });
+      gsap.to([textRef,signOutRef],{
+        opacity: 0,
+        duration: 0.5,
+        ease: Power3.easeInOut
+      })
     }
   });
 
@@ -40,13 +54,14 @@ const Profile = ({ currentUser }) => {
         <User className="profile__logo__raw" />
       </div>
       <div className="profile__dropdown" ref={(el) => (dropdownRef = el)}>
-        <li className="profile__dropdown__currentuser">
+        <li className="profile__dropdown__currentuser" ref={(el) => (textRef = el)}>
           signed in as{" "}
           {currentUser.displayName
             ? currentUser.displayName
             : currentUser.email}
         </li>
         <span
+        ref={(el) => (signOutRef = el)}
           className="profile__dropdown__signout"
           onClick={() => auth.signOut()}
           title={`signed In as: ${currentUser.email}`}
