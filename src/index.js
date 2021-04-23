a5848b2ff97b66625354928b354f74bf0d9f2208
armedev <epiratesdev@gmail.com>
2021-04-23 21:15:43 +0530

    added scroll to top

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 
 import App from './App.jsx';
+import ScrollTop from './components/scrollTop/scroll-top.component';
 import { store } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
+        <ScrollTop />
         <App />
       </React.StrictMode>
     </BrowserRouter>
